857f24f747ebf18c98b25dac9edb3e28abe887d8
TicianoFilho <ticianofilho@gmail.com>
2022-11-14 19:35:38 -0300

    fix: add @Onedecorator

--- src/entities/Unidade.ts
+++ src/entities/Unidade.ts
@@ -1,9 +1,11 @@
-import { Entity } from 'typeorm';
+/* eslint-disable indent */
+import { Entity, OneToMany } from 'typeorm';
 import { Agendamento } from './Agendamento';
 import { PessoaJuridica } from './PessoaJuridica';
 
 @Entity('unidade')
 export class Unidade extends PessoaJuridica {
 
+  @OneToMany(() => Agendamento, agendamento => agendamento.unidade)
   agendamentos: Agendamento[];
 }
