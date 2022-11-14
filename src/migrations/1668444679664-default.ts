d3a2c56a9cf87514031dea73cbe14cb6985a1e24
TicianoFilho <ticianofilho@gmail.com>
2022-11-14 14:22:08 -0300

    chore: add script migration:run

--- src/migrations/1668444679664-default.ts
+++ src/migrations/1668444679664-default.ts
@@ -1,7 +1,10 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable indent */
+/* eslint-disable quotes */
 import { MigrationInterface, QueryRunner } from "typeorm";
 
 export class default1668444679664 implements MigrationInterface {
-    name = 'default1668444679664'
+    name = 'default1668444679664';
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`CREATE TABLE "paciente" ("id" SERIAL NOT NULL, "nome" text NOT NULL, "cpf" text NOT NULL, "endereco" text NOT NULL, "telefone" character varying, "email" character varying, "ativo" boolean NOT NULL, CONSTRAINT "PK_cbcb7985432e4b49d32c5243867" PRIMARY KEY ("id"))`);
