da5f6244cba1a4557b8c2c88805a0163b14a1554
TicianoFilho <ticianofilho@gmail.com>
2022-11-18 15:03:42 -0300

    feat: add findOne method in pacienteController

--- src/controllers/PacienteController.ts
+++ src/controllers/PacienteController.ts
@@ -8,4 +8,28 @@ export class PacienteController {
     const pacientes: Paciente[] = await pacienteRepository.find();
     return res.status(200).json(pacientes);
   }
+
+  public async findOne(req: Request, res: Response) {
+    const { pacienteId } = req.params;
+    
+    try {    
+      const paciente = await pacienteRepository.findOneBy({ 
+        id: Number(pacienteId)
+      });
+
+      if (!paciente) {
+        return res.status(404).json({
+          message: `Paciente de código ${ pacienteId } não existe.`
+        });
+      }
+
+      return res.status(200).json(paciente); 
+
+    } catch (error: any) {
+      res.status(500).json({
+        message: 'Ocorreu algum erro no lado do servidor.',
+        error: error.message
+      });     
+    }
+  }
 }
--- src/routes/pacienteRoutes.ts
+++ src/routes/pacienteRoutes.ts
@@ -4,5 +4,6 @@ import { PacienteController } from '../controllers/PacienteController';
 const router = Router();
 
 router.get('/pacientes', new PacienteController().findAll);
+router.get('/pacientes/:pacienteId', new PacienteController().findOne);
 
 export default router;
